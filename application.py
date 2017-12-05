965587484868eaf4002f967641cdf29a4a7a5692
Jake Steeves <jsteeves@college.harvard.edu>
2017-12-04 19:13:17 -0500

    added logging

--- application.py
+++ application.py
@@ -1,7 +1,28 @@
 from flask import Flask, render_template, request
+import datetime
 
 application = Flask(__name__)
 
 @application.route("/")
 def index():
-    return "Hello!"
+    with open("static/messages.txt", "r") as f:
+        text = f.read()
+    f.closed
+    return text
+
+@application.route("/log"):
+    try:
+        message = request.args.get("msg")
+    except:
+        return "no message"
+    else:
+        addr = request.remote_addr
+        timestamp = datetime.datetime.now().strftime("%H:%M %m/%d/%y")
+        with open("static/messages.txt", "a+") as f:
+            f.write(addr + " - " + timestamp + "\n")
+            f.write(msg + "\n\n")
+        f.closed
+    
+        return "message logged"
+        
+    
